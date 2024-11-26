bc4a14369276309beb88d7c4f499495338ab1dff
ShkSchneider <shkschneider@noreply.codeberg.org>
2024-11-26 18:06:13 +0100

    split api and routes

--- api.go
+++ api.go
@@ -34,143 +34,40 @@ type (
 )
 
 func NewApi(db *Database) (*Api, error) {
-	api := echo.New()
+	var api Api = Api { *echo.New() }
 	api.Debug = DEBUG
-	{
-		api.Pre(middleware.NonWWWRedirect())
-		api.Pre(middleware.RemoveTrailingSlash())
-		api.Use(func(next echo.HandlerFunc) echo.HandlerFunc {
-			return func(c echo.Context) error {
-				var apiRequest ApiRequest
-				if err := c.Bind(&apiRequest) ; err != nil {
-					return c.NoContent(http.StatusBadRequest)
-				}
-				return next(&LocalContext { c, apiRequest })
+	// Middlewares
+	api.Pre(middleware.NonWWWRedirect())
+	api.Pre(middleware.RemoveTrailingSlash())
+	api.Use(func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			var apiRequest ApiRequest
+			if err := c.Bind(&apiRequest) ; err != nil {
+				return c.NoContent(http.StatusBadRequest)
 			}
-		})
-		api.Use(middleware.LoggerWithConfig(middleware.LoggerConfig {
-			Skipper: middleware.DefaultSkipper,
-			Format: "${time_rfc3339} ${method} ${uri} [${status}] ${error}\n",
-		}))
-	}
-	{
-		api.Renderer = &Template {
-		    templates: template.Must(template.ParseGlob(TEMPLATES)),
+			return next(&LocalContext { c, apiRequest })
 		}
-		api.HTTPErrorHandler = func(err error, c echo.Context) {
-			code := http.StatusNotAcceptable
-			if he, ok := err.(*echo.HTTPError) ; ok {
-				code = he.Code
-			}
-			c.Render(code, tERROR, struct{}{})
+	})
+	api.Use(middleware.LoggerWithConfig(middleware.LoggerConfig {
+		Skipper: middleware.DefaultSkipper,
+		Format: "${time_rfc3339} ${method} ${uri} [${status}] ${error}\n",
+	}))
+	// Rendering
+	api.Renderer = &Template {
+	    templates: template.Must(template.ParseGlob(TEMPLATES)),
+	}
+	api.HTTPErrorHandler = func(err error, c echo.Context) {
+		code := http.StatusNotAcceptable
+		if he, ok := err.(*echo.HTTPError) ; ok {
+			code = he.Code
 		}
+		c.Render(code, tERROR, struct{}{})
 	}
-	{
-		api.File("/favicon-light.ico", "html/favicon-light.ico")
-		api.File("/favicon-dark.ico", "html/favicon-dark.ico")
-		api.GET("/", func(_c echo.Context) error {
-			c := _c.(*LocalContext)
-			parents, _ := db.getParents()
-			if parents == nil {
-				return c.ok(http.StatusOK, tINDEX, []ApiResponse{})
-			}
-			var responses []ApiResponse = *new([]ApiResponse)
-			for _, parent := range *parents {
-				children, _ := db.getChildren(&parent)
-				responses = append(responses, ApiResponse {
-					Parent: nil,
-					Task: &parent,
-					Children: children,
-				})
-			}
-			return c.ok(http.StatusOK, tINDEX, &responses)
-		})
-		api.GET("/_new", func(_c echo.Context) error {
-			c := _c.(*LocalContext)
-			return c.ok(http.StatusOK, tFORM, nil)
-		})
-		api.POST("/_new", func(_c echo.Context) error {
-			c := _c.(*LocalContext)
-			task, err := db.add("", c.apiRequest.Title, c.apiRequest.Description)
-			if err != nil { return c.ko(http.StatusInternalServerError) }
-			return c.redirect("/" + task.Slug)
-		})
-		api.GET("/:slug", func(_c echo.Context) error {
-			c := _c.(*LocalContext)
-			task, err := db.get(c.apiRequest.Slug)
-			if err != nil { return c.ko(http.StatusNotFound) }
-			parent, _ := db.getParent(task)
-			children, _ := db.getChildren(task)
-			return c.ok(http.StatusFound, tTASK, ApiResponse {
-				Parent: parent,
-				Task: task,
-				Children: children,
-			})
-		})
-		api.GET("/:slug/_edit", func(_c echo.Context) error {
-			c := _c.(*LocalContext)
-			task, err := db.get(c.apiRequest.Slug)
-			if err != nil { return c.ko(http.StatusNotFound) }
-			return c.ok(http.StatusFound, tFORM, ApiResponse {
-				Task: task,
-			})
-		})
-		api.POST("/:slug/_edit", func(_c echo.Context) error {
-			c := _c.(*LocalContext)
-			task, err := db.get(c.apiRequest.Slug)
-			if err != nil { return c.ko(http.StatusNotFound) }
-			task.Title = c.apiRequest.Title
-			task.Description = c.apiRequest.Description
-			db.update(*task)
-			return c.ok(http.StatusFound, tTASK, ApiResponse {
-				Task: task,
-			})
-		})
-		api.GET("/:slug/_new", func(_c echo.Context) error {
-			c := _c.(*LocalContext)
-			parent, err := db.get(c.apiRequest.Slug)
-			if err != nil { return c.ko(http.StatusNotFound) }
-			return c.ok(http.StatusOK, tFORM, ApiResponse {
-				Parent: parent,
-			})
-		})
-		api.POST("/:slug/_new", func(_c echo.Context) error {
-			c := _c.(*LocalContext)
-			task, err := db.add(c.apiRequest.Slug, c.apiRequest.Title, c.apiRequest.Description)
-			if err != nil { return c.ko(http.StatusNotFound) }
-			return c.redirect("/" + task.Slug)
-		})
-		api.GET("/:slug/:progress", func(_c echo.Context) error {
-			c := _c.(*LocalContext)
-			task, err := db.get(c.apiRequest.Slug)
-			if err != nil { return c.ko(http.StatusNotFound) }
-			parent, _ := db.getParent(task)
-			task.Progress = c.apiRequest.Progress
-			db.update(*task)
-			return c.redirect("/" + parent.Slug)
-		})
-		api.GET("/:slug/_delete", func(_c echo.Context) error {
-			c := _c.(*LocalContext)
-			task, err := db.get(c.apiRequest.Slug)
-			if err != nil { return c.ko(http.StatusNotFound) }
-			parent, _ := db.getParent(task)
-			db.remove(*task)
-			if parent != nil {
-				return c.redirect("/" + parent.Slug)
-			} else {
-				return c.redirect("/")
-			}
-		})
-		api.GET("/_json", func(_c echo.Context) error {
-			c := _c.(*LocalContext)
-			if !DEBUG {
-				return c.ko(http.StatusForbidden)
-			}
-			tasks, _ := db.getAll()
-			return c.JSON(http.StatusOK, tasks)
-		})
-	}
-	return &Api { *api }, nil
+	// Routes
+	api.File("/favicon-light.ico", "html/favicon-light.ico")
+	api.File("/favicon-dark.ico", "html/favicon-dark.ico")
+	api.NewRoutes(db)
+	return &api, nil
 }
 
 func (api *Api) Run(addr string) {
---
+++ routes.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"github.com/labstack/echo/v4"
+	"net/http"
+)
+
+func (api *Api) NewRoutes(db *Database) {
+	api.File("/favicon-light.ico", "html/favicon-light.ico")
+	api.File("/favicon-dark.ico", "html/favicon-dark.ico")
+	api.GET("/", func(_c echo.Context) error {
+		c := _c.(*LocalContext)
+		parents, _ := db.getParents()
+		if parents == nil {
+			return c.ok(http.StatusOK, tINDEX, []ApiResponse{})
+		}
+		var responses []ApiResponse = *new([]ApiResponse)
+		for _, parent := range *parents {
+			children, _ := db.getChildren(&parent)
+			responses = append(responses, ApiResponse {
+				Parent: nil,
+				Task: &parent,
+				Children: children,
+			})
+		}
+		return c.ok(http.StatusOK, tINDEX, &responses)
+	})
+	api.GET("/_new", func(_c echo.Context) error {
+		c := _c.(*LocalContext)
+		return c.ok(http.StatusOK, tFORM, nil)
+	})
+	api.POST("/_new", func(_c echo.Context) error {
+		c := _c.(*LocalContext)
+		task, err := db.add("", c.apiRequest.Title, c.apiRequest.Description)
+		if err != nil { return c.ko(http.StatusInternalServerError) }
+		return c.redirect("/" + task.Slug)
+	})
+	api.GET("/:slug", func(_c echo.Context) error {
+		c := _c.(*LocalContext)
+		task, err := db.get(c.apiRequest.Slug)
+		if err != nil { return c.ko(http.StatusNotFound) }
+		parent, _ := db.getParent(task)
+		children, _ := db.getChildren(task)
+		return c.ok(http.StatusFound, tTASK, ApiResponse {
+			Parent: parent,
+			Task: task,
+			Children: children,
+		})
+	})
+	api.GET("/:slug/_edit", func(_c echo.Context) error {
+		c := _c.(*LocalContext)
+		task, err := db.get(c.apiRequest.Slug)
+		if err != nil { return c.ko(http.StatusNotFound) }
+		return c.ok(http.StatusFound, tFORM, ApiResponse {
+			Task: task,
+		})
+	})
+	api.POST("/:slug/_edit", func(_c echo.Context) error {
+		c := _c.(*LocalContext)
+		task, err := db.get(c.apiRequest.Slug)
+		if err != nil { return c.ko(http.StatusNotFound) }
+		task.Title = c.apiRequest.Title
+		task.Description = c.apiRequest.Description
+		db.update(*task)
+		return c.ok(http.StatusFound, tTASK, ApiResponse {
+			Task: task,
+		})
+	})
+	api.GET("/:slug/_new", func(_c echo.Context) error {
+		c := _c.(*LocalContext)
+		parent, err := db.get(c.apiRequest.Slug)
+		if err != nil { return c.ko(http.StatusNotFound) }
+		return c.ok(http.StatusOK, tFORM, ApiResponse {
+			Parent: parent,
+		})
+	})
+	api.POST("/:slug/_new", func(_c echo.Context) error {
+		c := _c.(*LocalContext)
+		task, err := db.add(c.apiRequest.Slug, c.apiRequest.Title, c.apiRequest.Description)
+		if err != nil { return c.ko(http.StatusNotFound) }
+		return c.redirect("/" + task.Slug)
+	})
+	api.GET("/:slug/:progress", func(_c echo.Context) error {
+		c := _c.(*LocalContext)
+		task, err := db.get(c.apiRequest.Slug)
+		if err != nil { return c.ko(http.StatusNotFound) }
+		parent, _ := db.getParent(task)
+		task.Progress = c.apiRequest.Progress
+		db.update(*task)
+		return c.redirect("/" + parent.Slug)
+	})
+	api.GET("/:slug/_delete", func(_c echo.Context) error {
+		c := _c.(*LocalContext)
+		task, err := db.get(c.apiRequest.Slug)
+		if err != nil { return c.ko(http.StatusNotFound) }
+		parent, _ := db.getParent(task)
+		db.remove(*task)
+		if parent != nil {
+			return c.redirect("/" + parent.Slug)
+		} else {
+			return c.redirect("/")
+		}
+	})
+	api.GET("/_json", func(_c echo.Context) error {
+		c := _c.(*LocalContext)
+		if !DEBUG {
+			return c.ko(http.StatusForbidden)
+		}
+		tasks, _ := db.getAll()
+		return c.JSON(http.StatusOK, tasks)
+	})
+}
